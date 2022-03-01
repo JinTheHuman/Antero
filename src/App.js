cbc9ec0f86a495211e1137de2a94a0b116887bb7
Jin Sunwoo <jinsunwoo500@gmail.com>
2022-03-01 15:23:48 +1100

    adding Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from "./Components/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import './Header.css';
+
+const Header = ({stage}) => {
+  return (
+    <>
+      <div className="header">
+        <button className='gonext'>{stage}</button>
+      </div>
+      <div className='stage-bar'>
+        <a className={'brainstorm-stage', (stage=='Finish Retro' ? 'Hi': '')} >Brainstorm</a>
+        <span>-></span>
+        <a className='brainstorm-stage'>Group & Vote</a>
+        <span>-></span>
+        <a className='brainstorm-stage'>Add Action Items</a>
+        <span>-></span>
+        <a className='brainstorm-stage'>Done</a>
+      </div>
+    </>
+
+  )
+}
+Header.defaultProps = {
+  stage: 'Finish Retro',
+}
+
+
+export default Header
