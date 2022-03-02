ebd6b4bb2cec5ba0b60da091e06b2a3f20cff820
Jin Sunwoo <jinsunwoo500@gmail.com>
2022-03-02 15:27:56 +1100

    adding arrays and chaning header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import Header from "./Components/Header";
+import React, {useState} from 'react'
 
 function App() {
+
+  const [stage, setStage] = useState(1);
+  const [improvements, setImprovements] = useState([{id: 0, text: "test1", likes: 0}]);
+  const [questions, setQuestions] = useState([{id: 1, text: "test1", likes: 0}]);
+  const [workedWell, setWorkedWell] = useState([{id: 2, text: "test1", likes: 0}]);
+  const [toDo, setToDo] = useState([]);
+
+  
   return (
     <div className="App">
-      <Header/>
+      <Header stage={stage}/>
+      <div className="Columns">
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+        <div>Four</div>
+      </div>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,44 @@
 import './Header.css';
 
 const Header = ({stage}) => {
+  var buttonText;
+  switch(stage) {
+    case 1:
+      buttonText = "Group & vote comments";
+      break;
+    case 2:
+      buttonText = "Discuss and add action items";
+      break;
+    case 3:
+      buttonText = "Finish retro";
+      break;
+    case 3:
+      buttonText = "Start new retro";
+      break;
+    default:
+      buttonText = "Start new retro";
+  }
+  
   return (
     <>
       <div className="header">
-        <button className='gonext'>{stage}</button>
+        <span className='gonext'>{buttonText}</span>
       </div>
       <div className='stage-bar'>
-        <a className={'brainstorm-stage', (stage=='Finish Retro' ? 'Hi': '')} >Brainstorm</a>
-        <span>-></span>
-        <a className='brainstorm-stage'>Group & Vote</a>
-        <span>-></span>
-        <a className='brainstorm-stage'>Add Action Items</a>
-        <span>-></span>
-        <a className='brainstorm-stage'>Done</a>
+        <a className={'brainstorm-stage', (stage==1 ? 'current': '')} >Brainstorm</a>
+        <span className='arrow'>-></span>
+        <a className={'group-stage', (stage==2 ? 'current': '')}>Group & Vote</a>
+        <span className='arrow'>-></span>
+        <a className={'add-stage', (stage==3 ? 'current': '')}>Add Action Items</a>
+        <span className='arrow'>-></span>
+        <a className={'done-stage', (stage==4 ? 'current': '')}>Done</a>
       </div>
     </>
 
   )
 }
 Header.defaultProps = {
-  stage: 'Finish Retro',
+  stage: 1,
 }
 
 
