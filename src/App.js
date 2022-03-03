e0e1b5446e235c106cd40d6d55e6b057fd9a194d
Jin Sunwoo <jinsunwoo500@gmail.com>
2022-03-03 21:32:21 +1100

    added some comments

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Header from "./Components/Header";
 import React, {useState} from 'react'
 
 function App() {
-
+  // This will hold our stage, some functionalities cannot be performed depending on the stage
   const [stage, setStage] = useState(1);
+  
+  // Holds the comments four each of our four columns
   const [improvements, setImprovements] = useState([{id: 0, text: "test1", likes: 0}]);
   const [questions, setQuestions] = useState([{id: 1, text: "test1", likes: 0}]);
   const [workedWell, setWorkedWell] = useState([{id: 2, text: "test1", likes: 0}]);
   const [toDo, setToDo] = useState([]);
 
+  // Switches to the next stage
   const changeState = () => {
     if (stage === 4) {
       setStage(1);
