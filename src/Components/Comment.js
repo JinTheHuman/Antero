907a6a6443a6f42fe435be7292f2dba02d39b734
williamyzhong <79193227+williamyzhong@users.noreply.github.com>
2022-03-21 13:44:04 +1100

    attempted checkbox style fix

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
-import {FaTimes, FaThumbsUp} from 'react-icons/fa'
-import "./Comment.css"
+import React, { useState } from "react";
+import { FaTimes, FaThumbsUp } from "react-icons/fa";
+import "./Comment.css";
 
-const Comment = ({comment, onClicked, onDelete}) => {
+const Comment = ({ comment, onClicked, onDelete }) => {
   const [isShown, setIsShown] = useState(false);
-  
+
   return (
-    <div className='Comment'
+    <div
+      className="Comment"
       // Changes if the cross is visible
-        onMouseOver={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}>
-        
-        {isShown || comment.column === "toDo" ? <p><FaTimes class='cross' style={{cursor:'pointer'}} onClick={()=>onDelete(comment.id, comment.column)}/></p> : 
-            <p>#{comment.id}</p>
-        }
+      onMouseOver={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      {isShown || comment.column === "toDo" ? (
+        <p className="leftObj">
+          <FaTimes
+            class="cross"
+            style={{ cursor: "pointer" }}
+            onClick={() => onDelete(comment.id, comment.column)}
+          />
+        </p>
+      ) : (
+        <p className="leftObj">#{comment.id}</p>
+      )}
 
-        {comment.column === "toDo" ? 
-        <h3 class="check-box"><input type="checkbox"/>{comment.text}</h3>
-        : 
-        <h3>{comment.text}</h3>}
+      {comment.column === "toDo" ? (
+        <>
+          <input type="checkbox" />{" "}
+          <h3 className="check-box">{comment.text}</h3>
+        </>
+      ) : (
+        <h3 class="text">{comment.text}</h3>
+      )}
 
-        {comment.column !== "toDo" && 
-        <p class='likes'>
-            <FaThumbsUp class="like" onClick={()=>onClicked(comment.id, comment.column)}/>
-            {comment.likes}
-        </p>}
+      {comment.column !== "toDo" && (
+        <p class="likes">
+          <FaThumbsUp
+            class="like"
+            onClick={() => onClicked(comment.id, comment.column)}
+          />
+          {comment.likes}
+        </p>
+      )}
     </div>
-  )
-}
-export default Comment
+  );
+};
+export default Comment;
