eeb382c11b98f8e26748b365f2027167031825dd
Laura <laurawen003@gmail.com>
2022-03-28 16:21:11 +1100

    Added an export button in the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,8 @@
 import './Header.css';
 
-const Header = ({stage, changeState}) => {
+const Header = ({ stage, changeState }) => {
   var buttonText;
-  switch(stage) {
+  switch (stage) {
     case 1:
       buttonText = "Group & vote comments";
       break;
@@ -18,25 +18,30 @@ const Header = ({stage, changeState}) => {
     default:
       buttonText = "Start new retro";
   }
-  
+
   return (
     <>
       <div className="header">
-          <img src="" alt="Insert Logo Here" />
-          <div style={{display: "inline-block"}} className="left-side-header">
+        <img src="" alt="Insert Logo Here" />
+        <div style={{ display: "inline-block" }} className="left-side-header">
+          <span className='gonext' onClick={changeState}>
+            {buttonText}
+          </span>
+          {stage === 4 && (
             <span className='gonext' onClick={changeState}>
-              {buttonText}
+              {"Export"}
             </span>
-          </div>
+          )}
+        </div>
       </div>
       <div className='stage-bar'>
-        <a className={'brainstorm-stage', (stage==1 ? 'current': '')} >Brainstorm</a>
+        <a className={'brainstorm-stage', (stage == 1 ? 'current' : '')} >Brainstorm</a>
         <span className='arrow'>-></span>
-        <a className={'group-stage', (stage==2 ? 'current': '')}>Group & Vote</a>
+        <a className={'group-stage', (stage == 2 ? 'current' : '')}>Group & Vote</a>
         <span className='arrow'>-></span>
-        <a className={'add-stage', (stage==3 ? 'current': '')}>Add Action Items</a>
+        <a className={'add-stage', (stage == 3 ? 'current' : '')}>Add Action Items</a>
         <span className='arrow'>-></span>
-        <a className={'done-stage', (stage==4 ? 'current': '')}>Done</a>
+        <a className={'done-stage', (stage == 4 ? 'current' : '')}>Done</a>
       </div>
     </>
 
