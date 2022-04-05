269fbbc1ca7ade673a22a2fa2c251b27d8de4069
Jin Sunwoo <jinsunwoo500@gmail.com>
2022-04-05 17:03:33 +1000

    adding collabrative functioanlilyt likes adding delete

--- Client/src/App.js
+++ Client/src/App.js
@@ -19,56 +19,43 @@ function App() {
     return () => newSocket.close();
   }, [setSocket])
 
-  if (socket) {
-    socket.on("connection", (id) => {
-      console.log("You are conencted with ", id);
-      // updateMessage(receivedMessages.message);
-    });
 
-    socket.on("receive-columns", (columns) => {
-      console.log("Poop");
-      setImprovements(columns[0]);
-      setQuestions(columns[1]);
-      setToDo(columns[3]);
-      setWorkedWell(columns[2]);
-    })
-  }
   // SOCKET STUFF
 
   const [stage, setStage] = useState(1);
   const [improvements, setImprovements] = useState([
-    {
-      index: 0,
-      text: "test1",
-      likes: 0,
-      column: "improvements",
-      id: "droppable-0",
-    },
+    // {
+    //   index: 0,
+    //   text: "test1",
+    //   likes: 0,
+    //   column: "improvements",
+    //   id: "droppable-0",
+    // },
   ]);
   const [questions, setQuestions] = useState([
-    {
-      index: 1,
-      text: "test1",
-      likes: 0,
-      column: "questions",
-      id: "droppable-1",
-    },
+    // {
+    //   index: 1,
+    //   text: "test1",
+    //   likes: 0,
+    //   column: "questions",
+    //   id: "droppable-1",
+    // },
   ]);
   const [workedWell, setWorkedWell] = useState([
-    {
-      index: 2,
-      text: "test1",
-      likes: 0,
-      column: "workedWell",
-      id: "droppable-2",
-    },
-    {
-      index: 3,
-      text: "test2",
-      likes: 0,
-      column: "workedWell",
-      id: "droppable-3",
-    },
+    // {
+    //   index: 2,
+    //   text: "test1",
+    //   likes: 0,
+    //   column: "workedWell",
+    //   id: "droppable-2",
+    // },
+    // {
+    //   index: 3,
+    //   text: "test2",
+    //   likes: 0,
+    //   column: "workedWell",
+    //   id: "droppable-3",
+    // },
   ]);
   const [toDo, setToDo] = useState([]);
 
@@ -76,13 +63,61 @@ function App() {
 
   const [showConfirm, setShowConfirm] = useState(false);
 
+  const [clientId, setClientId] = useState(null);
+
+  if (socket) {
+    socket.on("connection", (id) => {
+      console.log("You are conencted with ", id);
+      setClientId(id);
+      // updateMessage(receivedMessages.message);
+    });
+
+    socket.on("receive-nextId", (nextIdSERVER) => {
+      setNextId(nextIdSERVER);
+    })
+
+    // socket.on("receive-likes", (columns) => {
+    //   console.log("recieving likes");
+    //   setImprovements(columns[0]);
+    //   setQuestions(columns[1]);
+    //   setToDo(columns[3]);
+    //   setWorkedWell(columns[2]);
+    // });
+
+    socket.on("receive-columns", (columns) => {
+      console.log("Receiving columsn");
+      setImprovements(columns[0]);
+      setQuestions(columns[1]);
+      setToDo(columns[3]);
+      setWorkedWell(columns[2]);
+    })
+
+    socket.on("receive-current-state", (SERVER) => {
+      console.log("receive", SERVER);
+      setStage(SERVER[0]);
+      setImprovements(SERVER[1]);
+      setQuestions(SERVER[2]);
+      setWorkedWell(SERVER[3]);
+      setToDo(SERVER[4]);
+      setNextId(SERVER[5]);
+    })
+
+    socket.on("receive-stage", (stageSERVER)=>{
+      console.log("receiving stage", stageSERVER);
+      setStage(stageSERVER);
+    })
+  }
+
   const changeState = (inputStage) => {
     console.log("this is called ", inputStage);
     if (inputStage === "new") {
+      socket.emit("change-stage", 1);
       setStage(1);
     } else if (inputStage === "back") {
+      socket.emit("change-stage", 4);
       setStage(4);
     } else {
+      socket.emit("change-stage", stage + 1);
       setStage(stage + 1);
     }
     setShowConfirm(false);
@@ -107,6 +142,7 @@ function App() {
     const newImprovement = {
       index: nextId,
       id: "droppable-" + nextId,
+      likedClients: [],
       text: inputText,
       likes: 0,
       column: "improvements",
@@ -121,6 +157,7 @@ function App() {
     const newQuestion = {
       index: nextId,
       id: "droppable-" + nextId,
+      likedClients: [],
       text: inputText,
       likes: 0,
       column: "questions",
@@ -134,6 +171,7 @@ function App() {
     const newWorkedWell = {
       index: nextId,
       id: "droppable-" + nextId,
+      likedClients: [],
       text: inputText,
       likes: 0,
       column: "workedWell",
@@ -169,39 +207,42 @@ function App() {
       default:
         console.log("broken delete");
     }
+    socket.emit("deleteComment", id, column);
   };
 
   const likedComment = (id, column) => {
+    socket.emit("like-comment", ([id, column, clientId]));
     switch (column) {
       case "improvements":
         setImprovements(
           improvements.map((comment) =>
-            comment.id === id
-              ? { ...comment, likes: comment.likes + 1 }
+            (comment.id === id && !comment.likedClients.includes(clientId))
+              ? { ...comment, likes: comment.likes + 1, likedClients: [...comment.likedClients, clientId] }
               : comment
           )
         );
         console.log(improvements);
+        console.log("here");
         break;
       case "questions":
         setQuestions(
           questions.map((comment) =>
-            comment.id === id
-              ? { ...comment, likes: comment.likes + 1 }
+            (comment.id === id && !comment.likedClients.includes(clientId))
+              ? { ...comment, likes: comment.likes + 1, likedClients: [...comment.likedClients, clientId] }
               : comment
           )
         );
-        console.log(improvements);
+        console.log(questions);
         break;
       case "workedWell":
         setWorkedWell(
           workedWell.map((comment) =>
-            comment.id === id
-              ? { ...comment, likes: comment.likes + 1 }
+            (comment.id === id && !comment.likedClients.includes(clientId))
+              ? { ...comment, likes: comment.likes + 1, likedClients: [...comment.likedClients, clientId] }
               : comment
           )
         );
-        console.log(improvements);
+        console.log(workedWell);
         break;
       default:
         console.log("broken");
@@ -221,7 +262,7 @@ function App() {
       return;
     }
 
-    const column = this.state.columns[source.draoppableId];
+    const column = this.state.columns[source.droppableId];
     const newTaskIds = Array.from(column.Ids);
 
   }
--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const io = require("socket.io")(5000, {
 	cors: {
+    origin: ["http://localhost:3000"],
 		origin: ["http://localhost:3001"],
 	}
 });
@@ -16,12 +17,21 @@ let workedWell = [];
 
 let toDo = [];
 
+let nextId = 0;
+
 io.on("connection", socket => {
   console.log("hello member", socket.id);
   socket.emit("connection", socket.id);
+  socket.emit("receive-current-state", ([stage, improvements, questions, workedWell, toDo, nextId]));
+
+  socket.on("change-stage", (stageCLIENT)=>{
+    console.log("Stage changed to:", stage);
+    stage = stageCLIENT;
+    socket.broadcast.emit("receive-stage", stage);
+  })
 
   socket.on("addComment", (comment) => {
-    console.log("adding to columng", comment);
+    console.log("adding to column", comment);
     switch (comment.column) {
       case "workedWell":
         workedWell.push(comment);
@@ -39,9 +49,74 @@ io.on("connection", socket => {
       default:
         break;
     }
+    nextId += 1;
+    socket.broadcast.emit("receive-nextId", nextId);
+    socket.broadcast.emit("receive-columns", [improvements, questions, workedWell, toDo]);
+  })
+  
+  socket.on("deleteComment", (id, column) => {
+    console.log("deleting comment now", column);    
+    switch (column) {
+      case "workedWell":
+        workedWell = workedWell.filter((comment) => comment.id !== id);
+        console.log(workedWell);
+        break;
+      case "questions":
+        questions = questions.filter((comment) => comment.id !== id);
+        break;
+      case "improvements":
+        improvements = improvements.filter((comment) => comment.id !== id);
+        break;
+      case "todo":
+        toDo = toDo.filter((comment) => comment.id !== id);
+        break;
+      default:
+        console.log("default");
+        break;
+    }
     socket.broadcast.emit("receive-columns", [improvements, questions, workedWell, toDo]);
   })
 
-
+  socket.on("like-comment", (client) => {
+    console.log("liked", client[0]);
+    let idCLIENT = client[0]
+    let clientId = client[2]
+    switch (client[1]) {
+      case "improvements":
+        for(let i = 0; i < improvements.length; i++){
+          if(improvements[i].id == idCLIENT && !(improvements[i].likedClients.includes(clientId))){
+              console.log("like is updated");
+              improvements[i].likes += 1;
+              improvements[i].likedClients.push(clientId);
+              break; 
+            }
+        }
+        console.log("after liking in imporvements", improvements);
+        break;
+      case "questions":
+        for(var i in questions){
+          if(questions[i].id == idCLIENT && !(questions[i].likedClients.includes(clientId))){
+              questions[i].likes += 1;
+              questions[i].likedClients.push(clientId);
+              break; 
+            }
+        }
+        break;
+      case "workedWell":
+        for(var i in workedWell){
+          if(workedWell[i].id == idCLIENT && !(workedWell[i].likedClients.includes(clientId))){
+              workedWell[i].likes += 1;
+              workedWell[i].likedClients.push(clientId);
+              break; 
+            }
+        }
+        break;
+      default:
+        console.log("broken");
+    }
+    
+    socket.broadcast.emit("receive-columns", [improvements, questions, workedWell, toDo]);
+    // socket.broadcast.emit("receive-likes", []);
+  })
   
 })
